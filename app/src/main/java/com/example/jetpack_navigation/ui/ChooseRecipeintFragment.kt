f26a48e91250babbc350654de659db0083e447aa
ruifernando7 <rui.fernando7.rf@gmail.com>
2019-09-28 14:23:42 +0700

    Adding navigation to other fragment

--- app/src/main/java/com/example/jetpack_navigation/ui/ChooseRecipeintFragment.kt
+++ app/src/main/java/com/example/jetpack_navigation/ui/ChooseRecipeintFragment.kt
@@ -6,13 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.jetpack_navigation.R
+import kotlinx.android.synthetic.main.fragment_choose_recipeint.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class ChooseRecipeintFragment : Fragment() {
+class ChooseRecipeintFragment : Fragment(), View.OnClickListener{
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,22 @@ class ChooseRecipeintFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_choose_recipeint, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        next_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+
+    }
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.next_btn -> {
+                navController.navigate(R.id.action_chooseRecipeintFragment_to_specifyAmountFragment)
+            }
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
     }
 
 
--- app/src/main/java/com/example/jetpack_navigation/ui/SpecifyAmountFragment.kt
+++ app/src/main/java/com/example/jetpack_navigation/ui/SpecifyAmountFragment.kt
@@ -6,13 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.jetpack_navigation.R
+import kotlinx.android.synthetic.main.fragment_specify_amount.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class SpecifyAmountFragment : Fragment() {
+class SpecifyAmountFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,20 @@ class SpecifyAmountFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_specify_amount, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        send_btn.setOnClickListener(this)
+        cancel_btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.send_btn -> {
+                navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment)
+            }
+            R.id.cancel_btn -> activity?.onBackPressed()
+        }
+    }
 
 }
