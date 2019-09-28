723633e3e2e8f255c951f99c294e169f05da05b2
ruifernando7 <rui.fernando7.rf@gmail.com>
2019-09-28 13:26:00 +0700

    Able to move from one fragment to other fragement using nav controller

--- app/src/main/java/com/example/jetpack_navigation/ui/MainFragment.kt
+++ app/src/main/java/com/example/jetpack_navigation/ui/MainFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 import com.example.jetpack_navigation.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +22,22 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view_transactions_btn.setOnClickListener(this)
+        send_money_btn.setOnClickListener(this)
+        view_balance_btn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.view_transactions_btn -> navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment)
+            R.id.send_money_btn -> navController.navigate(R.id.action_mainFragment_to_chooseRecipeintFragment2)
+            R.id.view_balance_btn -> navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment)
+        }
     }
 
 
